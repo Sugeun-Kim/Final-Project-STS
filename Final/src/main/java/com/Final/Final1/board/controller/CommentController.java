72b4b69ab9d6717cf2bb18e5a2c9f07658b5ac4a
Sugeun-Kim <rlatnrms856naver.com>
2022-09-22 19:56:46 +0900

    수근

---
+++ Final/src/main/java/com/Final/Final1/board/controller/CommentController.java
@@ -0,0 +1,22 @@
+package com.Final.Final1.board.controller;
+
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+//@Controller
+public class CommentController {
+	
+//	@Autowired
+//	CommentService commentService;
+//	
+//	 @ResponseBody
+//	 @RequestMapping("Comment/insert")
+//	   public void insert(CommentDTO dto) {
+//		 commentService.insert(dto);
+//	   }
+//	
+}
